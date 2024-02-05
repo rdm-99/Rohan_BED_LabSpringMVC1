e03fefd2f54847a71dbcd7b55b587607686a8d3f
Rohan Deb Mondal <95281095+rdm-99@users.noreply.github.com>
2024-02-05 23:06:34 +0530

    Add files via upload

--- Employee-Management-System/src/main/java/com/learning/app/service/impl/EmployeeServiceImpl.java
+++ Employee-Management-System/src/main/java/com/learning/app/service/impl/EmployeeServiceImpl.java
@@ -1,1 +1,54 @@
+package com.learning.app.service.impl;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.learning.app.dao.EmployeeDao;
+import com.learning.app.models.Employee;
+import com.learning.app.service.EmployeeService;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    @Autowired
+    EmployeeDao employeeDao;
+
+    public void createEmployee(Employee employee) {
+        try {
+            employeeDao.saveAndFlush(employee);
+        } catch (Exception e) {
+            String msg = "CreateEmployeeException: " + e.getMessage();
+            throw new RuntimeException(msg);
+        }
+    }
+
+    public Employee getEmployeeById(Long employeeId) {
+        try {
+            return employeeDao.findById(employeeId)
+                    .orElseThrow(() -> new IllegalArgumentException("Not a valid employeeId(" + employeeId + ")"));
+        } catch (Exception e) {
+            String msg = "GetEmployeeByIdException: " + e.getMessage();
+            throw new RuntimeException(msg);
+        }
+    }
+
+    public List<Employee> fetchAllEmployees() {
+        try {
+            return employeeDao.findAll();
+        } catch (Exception e) {
+            String msg = "FetchAllEmployeesException: " + e.getMessage();
+            throw new RuntimeException(msg);
+        }
+    }
+
+    public void deleteEmployeeById(Long employeeId) {
+        try {
+            employeeDao.deleteById(employeeId);
+        } catch (Exception e) {
+            String msg = "DeleteEmployeeByIdException: " + e.getMessage();
+            throw new RuntimeException(msg);
+        }
+    }
+}
