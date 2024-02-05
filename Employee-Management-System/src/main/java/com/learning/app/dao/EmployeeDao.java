8a294773a5acb969891823692f84b0bb4d4c0437
Rohan Deb Mondal <95281095+rdm-99@users.noreply.github.com>
2024-02-05 23:01:29 +0530

    Add files via upload

--- Employee-Management-System/src/main/java/com/learning/app/dao/EmployeeDao.java
+++ Employee-Management-System/src/main/java/com/learning/app/dao/EmployeeDao.java
@@ -1,1 +1,8 @@
+package com.learning.app.dao;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.learning.app.models.Employee;
+
+public interface EmployeeDao extends JpaRepository<Employee, Long> {
+}
