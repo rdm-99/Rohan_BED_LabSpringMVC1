274c85aee35fa5332e196d7bfe217c49f51611a0
Rohan Deb Mondal <95281095+rdm-99@users.noreply.github.com>
2024-02-05 22:51:10 +0530

    Add files via upload

--- Employee-Management-System/src/test/java/com/learning/app/EmployeeManagementSystemApplicationTests.java
+++ Employee-Management-System/src/test/java/com/learning/app/EmployeeManagementSystemApplicationTests.java
@@ -1,1 +1,13 @@
+package com.learning.app;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
