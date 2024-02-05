7e0a2308cad343a52fa49029cc2a1f7f8151ac5f
Rohan Deb Mondal <95281095+rdm-99@users.noreply.github.com>
2024-02-05 23:04:29 +0530

    Add files via upload

--- Employee-Management-System/src/main/java/com/learning/app/models/Employee.java
+++ Employee-Management-System/src/main/java/com/learning/app/models/Employee.java
@@ -1,1 +1,25 @@
+package com.learning.app.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String emailAddress;
+}
