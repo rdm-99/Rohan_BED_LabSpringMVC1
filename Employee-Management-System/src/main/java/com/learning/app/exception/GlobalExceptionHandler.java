393a46b1ff00148d1331bf7e923d5af9119887c6
Rohan Deb Mondal <95281095+rdm-99@users.noreply.github.com>
2024-02-05 23:03:02 +0530

    Add files via upload

--- Employee-Management-System/src/main/java/com/learning/app/exception/GlobalExceptionHandler.java
+++ Employee-Management-System/src/main/java/com/learning/app/exception/GlobalExceptionHandler.java
@@ -1,1 +1,16 @@
+package com.learning.app.exception;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public String exceptionHandler(Model model, Exception ex){
+        String msg = ex.getMessage();
+        model.addAttribute("error", msg);
+        return "error";
+    }
+}
